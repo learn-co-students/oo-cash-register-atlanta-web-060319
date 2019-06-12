5ca64fad33cc3b17a33c3bc6506ba2b1b821bb21
github username <github email address>
2019-06-12 16:48:28 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+   
 
+    def initialize(total, employee_discount=0)
+        @total = total
+        @employee_discount = employee_discount
+    end
+
+   def total
+    self.total
+   end
+
+   def add_item(title, price)
+
+
+end
